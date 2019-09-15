418bace9b3280df365d63c0a99824ee01a1fb319
Hendrik Knoche <hendrik.knoche@gmail.com>
2019-09-15 17:52:51 +0200

    added raw data to plots and violin shapes for distribution

--- experiment-data/analysisSelfReports.R
+++ experiment-data/analysisSelfReports.R
@@ -10,6 +10,8 @@ require(reshape2)
 require(lme4)
 require(here)
 require(coin)
+require(sqldf)
+
 #verify with excluding PID=2 (only got 9/20 instead of 10/20)
 #setwd
 setwd(here::here("experiment-data"))
@@ -75,16 +77,58 @@ ggplot(dsm, aes(x=dsm$shamRate, y=dsm$FrustNormalized)) +
   xlab("Fabrication Rate (%)") +
   ylab("Frustration")
 
+# Plot Sham Rate vs Frustration w/ Error Bars (95% Confidence Interval) + raw data
+data$shamRate = factor(data$shamRate, levels=c("0", "15", "30"))
+data$FrustNormalized = (data$FrustEpisode-1)/6
+dsm = summarySE(data, measurevar="FrustNormalized", groupvars=c("shamRate"))
+ggplot(dsm, aes(x=dsm$shamRate, y=dsm$FrustNormalized)) +
+  geom_violin(data=data,aes(factor(shamRate),FrustNormalized),trim=FALSE)+
+  geom_errorbar(aes(ymin=dsm$FrustNormalized-dsm$ci, ymax=dsm$FrustNormalized+dsm$ci), width=.1, size=.5) +
+  geom_point(size=3.25)+ 
+  geom_line(aes(group=1), size=1.25) +
+  geom_jitter(data=data,aes(factor(shamRate),FrustNormalized),width = .2,alpha=.2)+
+  ylim(0,1) +
+  theme_bw() +
+  theme(text = element_text(size = 13)) +
+  xlab("Fabrication Rate (%)") +
+  ylab("Frustration")
+
 # Plot Sham Rate vs Perceived Control w/ Error Bars
 data$shamRate = factor(data$shamRate, levels=c("0", "15", "30"))
 data$controlNormalized = (data$controlEpisode-1)/6
 dsm = summarySE(data, measurevar="controlNormalized", groupvars=c("shamRate"))
 ggplot(dsm, aes(x=dsm$shamRate, y=dsm$controlNormalized)) +
   geom_errorbar(aes(ymin=dsm$controlNormalized-dsm$ci, ymax=dsm$controlNormalized+dsm$ci), width=.1, size=.5) +
-  geom_point(size=3.25) +
+  geom_point(size=3.25) + 
   geom_line(aes(group=1), size=1.25) +
   ylim(0,1) +
   theme_bw() +
   theme(text = element_text(size = 13)) +
   xlab("Fabrication Rate (%)") +
   ylab("Perceived Control")
+
+
+#Plot Sham Rate vs Perceived Control w/ Error Bars  with raw data
+ggplot(dsm, aes(x=dsm$shamRate, y=dsm$controlNormalized)) +
+  geom_violin(data=data,aes(factor(shamRate),controlNormalized),trim=FALSE)+
+  geom_errorbar(aes(ymin=dsm$controlNormalized-dsm$ci, ymax=dsm$controlNormalized+dsm$ci), width=.1, size=.5) +
+  geom_point(size=3.25) + 
+  geom_line(aes(group=1), size=1.25) +
+  #geom_point(data=data,aes(factor(shamRate),controlNormalized))+
+  geom_jitter(data=data,aes(factor(shamRate),controlNormalized),width = .2,alpha=.2)+
+  ylim(0,1) +
+  theme_bw() +
+  theme(text = element_text(size = 13)) +
+  xlab("Fabrication Rate (%)") +   ylab("Perceived Control")
+
+#check if perceived control is just inverse of Frustration, doesn't seem to be the case
+summary(step(lm(FrustEpisode~controlEpisode*as.factor(shamRate),data)))
+
+ggplot(data,aes(x=controlEpisode,y=FrustEpisode,colour=shamRate,group=shamRate,alpha=.3))+geom_point()+geom_jitter(width=.1)+theme_bw()+geom_smooth(method = "lm", se = FALSE)
+
+# comparison with people that had some sort of insight into what we were manipulating vs not
+sqldf("select ExpInsight, avg(ControlEpisode) from data group by expinsight")
+sqldf("select ExpInsight, avg(FrustEpisode) from data group by expinsight")
+sqldf("select ExpInsight, median(ControlEpisode) from data group by expinsight")
+sqldf("select ExpInsight, median(FrustEpisode) from data group by expinsight")
+
