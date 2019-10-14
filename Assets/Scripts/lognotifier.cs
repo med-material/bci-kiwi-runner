9e7fc08b2d03ca7e3c5fdd815fd933cd418d641d
MaximeHollebecq <maxime.hollebecq@viacesi.fr>
2019-10-14 11:01:43 +0200

    Add Lognotifier.cs Script
    This interface is link between the logger and the class which invoke the
    event.
    How it work : the event notify() can be called and take a dictionary with
    string and a list<string> as parameter.this dictionary is called by the
    logger who write the data to a file

---
+++ Assets/Scripts/lognotifier.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+[System.Serializable]
+public class MyEvent : UnityEvent<Dictionary<string, List<string>>>
+{
+
+}
+
+public class Lognotifier : MonoBehaviour
+{
+
+    public MyEvent m_MyEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frames
+    protected virtual void notify(Dictionary<string, List<string>> dico)
+    {
+
+        m_MyEvent.Invoke(dico);
+
+    }
+}
